60f9ad96c7cfa8629339277dae9f139aba834b2d
shiwoo.park <custardcream@kakao.com>
2023-01-29 03:20:21 +0900

    Config: gitignore

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-export interface ButtonProps {
-    label: string;
-}
-declare const Button: (props: ButtonProps) => import("@emotion/react/jsx-runtime").JSX.Element;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as _emotion_react_jsx_runtime from '@emotion/react/jsx-runtime';
-
-interface ButtonProps {
-    label: string;
-}
-declare const Button: (props: ButtonProps) => _emotion_react_jsx_runtime.JSX.Element;
-
-export { Button };
