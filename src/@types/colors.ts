206c1ee06d32dec101878e56a1f402c1358be681
shiwoo.park <custardcream@kakao.com>
2023-03-02 02:00:28 +0900

    Style: gray 팔레트 추가

--- src/@types/colors.ts
+++ src/@types/colors.ts
@@ -3,7 +3,17 @@ export type Colors = {
   black: string;
   primary: string;
   secondary: string;
+
   gray: string;
+  gray100: string;
+  gray200: string;
+  gray300: string;
+  gray400: string;
+  gray500: string;
+  gray600: string;
+  gray700: string;
+  gray800: string;
+  gray900: string;
 };
 
 export type ColorsType = keyof Colors;
--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -9,6 +9,15 @@ const colors: Colors = {
   primary: "#FFA500",
   secondary: "#5b52fb",
   gray: "#cccccc",
+  gray100: "#f8f9fa",
+  gray200: "#e9ecef",
+  gray300: "#dee2e6",
+  gray400: "#ced4da",
+  gray500: "#adb5bd",
+  gray600: "#6c757d",
+  gray700: "#495057",
+  gray800: "#343a40",
+  gray900: "#212529",
 } as const;
 
 /**
