417caefc8cb21fe6327b7dfc738e94e29fa1c220
shiwoo.park <custardcream@kakao.com>
2023-03-03 23:28:55 +0900

    Style: orange 컬러 팔레트 추가

--- src/@types/colors.ts
+++ src/@types/colors.ts
@@ -4,6 +4,17 @@ export type Colors = {
   primary: string;
   secondary: string;
 
+  orange: string;
+  orange100: string;
+  orange200: string;
+  orange300: string;
+  orange400: string;
+  orange500: string;
+  orange600: string;
+  orange700: string;
+  orange800: string;
+  orange900: string;
+
   gray: string;
   gray100: string;
   gray200: string;
